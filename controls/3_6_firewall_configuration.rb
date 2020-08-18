f16e979e0274cb4708efdab272f7fa5107a5f8bb
Adrian Mlodzianowski <amlodzianowski@logicworks.net>
2020-08-18 13:38:50 -0700

    ignore ipv4 loopback for 3.5.1.4

--- controls/3_6_firewall_configuration.rb
+++ controls/3_6_firewall_configuration.rb
@@ -110,7 +110,7 @@ control 'cis-dil-benchmark-3.5.1.4' do
   tag cis: 'distribution-independent-linux:3.5.1.4'
   tag level: 1
 
-  port.where { address !~ /^::1$/ }.ports.each do |port|
+  port.where { address !~ /^(127\.0\.0\.1|::1)$/ }.ports.each do |port|
     describe "Firewall rule should exist for port #{port}" do
       subject { ip6tables.retrieve_rules.any? { |s| s =~ /\s--(dport|dports) #{port}\s/ } }
       it { should be true }
