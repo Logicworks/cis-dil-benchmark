4d256f14f0a7ad570ae38d2bfb1a499698e7e2a0
Yury Gagarin <ygagarin@logicworks.net>
2020-08-28 16:11:21 -0700

    Updates section 5.2.16

--- controls/5_2_ssh_server_configuration.rb
+++ controls/5_2_ssh_server_configuration.rb
@@ -292,19 +292,19 @@ control 'cis-dil-benchmark-5.2.15' do
   end
 end
 
-# control 'cis-dil-benchmark-5.2.13' do
-#   title 'Ensure SSH Idle Timeout Interval is configured'
-#   desc  "The two options ClientAliveInterval and ClientAliveCountMax control the timeout of ssh sessions. When the ClientAliveInterval variable is set, ssh sessions that have no activity for the specified length of time are terminated. When the ClientAliveCountMax variable is set, sshd will send client alive messages at every ClientAliveInterval interval. When the number of consecutive client alive messages are sent with no response from the client, the ssh session is terminated. For example, if the ClientAliveInterval is set to 15 seconds and the ClientAliveCountMax is set to 3, the client ssh session will be terminated after 45 seconds of idle time.\n\nRationale: Having no timeout value associated with a connection could allow an unauthorized user access to another user's ssh session (e.g. user walks away from their computer and doesn't lock the screen). Setting a timeout value at least reduces the risk of this happening.. While the recommended setting is 300 seconds (5 minutes), set this timeout value based on site policy. The recommended setting for ClientAliveCountMax is 0. In this case, the client session will be terminated after 5 minutes of idle time and no keepalive messages will be sent."
-#   impact 1.0
+control 'cis-dil-benchmark-5.2.16' do
+  title 'Ensure SSH Idle Timeout Interval is configured (Scored)'
+  desc  "The two options ClientAliveInterval and ClientAliveCountMax control the timeout of ssh sessions. When the ClientAliveInterval variable is set, ssh sessions that have no activity for the specified length of time are terminated. When the ClientAliveCountMax variable is set, sshd will send client alive messages at every ClientAliveInterval interval. When the number of consecutive client alive messages are sent with no response from the client, the ssh session is terminated. For example, if the ClientAliveInterval is set to 15 seconds and the ClientAliveCountMax is set to 3, the client ssh session will be terminated after 45 seconds of idle time.\n\nRationale: Having no timeout value associated with a connection could allow an unauthorized user access to another user's ssh session (e.g. user walks away from their computer and doesn't lock the screen). Setting a timeout value at least reduces the risk of this happening. While the recommended setting is 300 seconds (5 minutes), set this timeout value based on site policy. The recommended setting for ClientAliveCountMax is 0. In this case, the client session will be terminated after 5 minutes of idle time and no keepalive messages will be sent."
+  impact 1.0
 
-#   tag cis: 'distribution-independent-linux:5.2.13'
-#   tag level: 1
+  tag cis: 'distribution-independent-linux:5.2.16'
+  tag level: 1
 
-#   describe sshd_config do
-#     its(:ClientAliveInterval) { should cmp <= 300 }
-#     its(:ClientAliveCountMax) { should cmp <= 3 }
-#   end
-# end
+  describe sshd_config do
+    its('ClientAliveInterval') { should cmp <= 300 }
+    its('ClientAliveCountMax') { should cmp <= 0 }
+  end
+end
 
 # control 'cis-dil-benchmark-5.2.14' do
 #   title 'Ensure SSH LoginGraceTime is set to one minute or less'
