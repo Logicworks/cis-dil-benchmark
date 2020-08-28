f0cad1ea8701d0efea9a0b1d01e82600374eef0f
Yury Gagarin <ygagarin@logicworks.net>
2020-08-28 14:42:35 -0700

    Updates section 5.2.15

--- controls/5_2_ssh_server_configuration.rb
+++ controls/5_2_ssh_server_configuration.rb
@@ -259,6 +259,39 @@ control 'cis-dil-benchmark-5.2.14' do
   end
 end
 
+control 'cis-dil-benchmark-5.2.15' do
+  title 'Ensure only strong Key Exchange algorithms are used (Scored)'
+  desc  "Key exchange is any method in cryptography by which cryptographic keys are exchanged between two parties, allowing use of a cryptographic algorithm.\n\nRationale: Key exchange methods that are considered weak should be removed. A key exchange method may be weak because too few bits are used, or the hashing algorithm is considered too weak. Using weak algorithms could expose connections to man-in-the-middle attacks"
+  impact 1.0
+
+  tag cis: 'distribution-independent-linux:5.2.15'
+  tag level: 1
+
+  describe sshd_config do
+    its('KexAlgorithms') { should_not be_nil }
+  end
+
+  ALLOWED_KEX_ALGORITHMS = [
+    'curve25519-sha256',
+    'curve25519-sha256@libssh.org',
+    'ecdh-sha2-nistp256',
+    'ecdh-sha2-nistp384',
+    'ecdh-sha2-nistp521',
+    'diffie-hellman-group-exchange-sha256',
+    'diffie-hellman-group16-sha512',
+    'diffie-hellman-group18-sha512',
+    'diffie-hellman-group14-sha256'
+  ].freeze
+
+  if sshd_config.KexAlgorithms
+    sshd_config.KexAlgorithms.split(',').each do |m|
+      describe m do
+        it { should be_in ALLOWED_KEX_ALGORITHMS }
+      end
+    end
+  end
+end
+
 # control 'cis-dil-benchmark-5.2.13' do
 #   title 'Ensure SSH Idle Timeout Interval is configured'
 #   desc  "The two options ClientAliveInterval and ClientAliveCountMax control the timeout of ssh sessions. When the ClientAliveInterval variable is set, ssh sessions that have no activity for the specified length of time are terminated. When the ClientAliveCountMax variable is set, sshd will send client alive messages at every ClientAliveInterval interval. When the number of consecutive client alive messages are sent with no response from the client, the ssh session is terminated. For example, if the ClientAliveInterval is set to 15 seconds and the ClientAliveCountMax is set to 3, the client ssh session will be terminated after 45 seconds of idle time.\n\nRationale: Having no timeout value associated with a connection could allow an unauthorized user access to another user's ssh session (e.g. user walks away from their computer and doesn't lock the screen). Setting a timeout value at least reduces the risk of this happening.. While the recommended setting is 300 seconds (5 minutes), set this timeout value based on site policy. The recommended setting for ClientAliveCountMax is 0. In this case, the client session will be terminated after 5 minutes of idle time and no keepalive messages will be sent."
