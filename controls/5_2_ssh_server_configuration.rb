f2d5e0039b2dfb3223fe512ed83413232a1e12aa
Yury Gagarin <ygagarin@logicworks.net>
2020-08-29 21:23:28 -0700

    Updates sections 5.2.22 5.2.23

--- controls/5_2_ssh_server_configuration.rb
+++ controls/5_2_ssh_server_configuration.rb
@@ -377,3 +377,29 @@ control 'cis-dil-benchmark-5.2.21' do
     its('AllowTcpForwarding') { should eq 'no' }
   end
 end
+
+control 'cis-dil-benchmark-5.2.22' do
+  title 'Ensure SSH MaxStartups is configured (Scored)'
+  desc  "The MaxStartups parameter specifies the maximum number of concurrent unauthenticated connections to the SSH daemon.\n\nRationale: To protect a system from denial of service due to a large number of pending authentication connection attempts, use the rate limiting function of MaxStartups to protect availability of sshd logins and prevent overwhelming the daemon."
+  impact 1.0
+
+  tag cis: 'distribution-independent-linux:5.2.22'
+  tag level: 1
+
+  describe sshd_config do
+    its('MaxStartups') { should eq '10:30:60' }
+  end
+end
+
+control 'cis-dil-benchmark-5.2.23' do
+  title 'Ensure SSH MaxSessions is set to 4 or less (Scored)'
+  desc  "The MaxSessions parameter specifies the maximum number of open sessions permitted from a given connection.\n\nRationale: To protect a system from denial of service due to a large number of concurrent sessions, use the rate limiting function of MaxSessions to protect availability of sshd logins and prevent overwhelming the daemon."
+  impact 1.0
+
+  tag cis: 'distribution-independent-linux:5.2.23'
+  tag level: 1
+
+  describe sshd_config do
+    its('MaxSessions') { should cmp <= 4 }
+  end
+end
